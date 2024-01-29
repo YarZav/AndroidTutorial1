3e3a0ed576c553133deca8327d5a22a4adacd0d9
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2024-01-29 12:48:34 +0300

    Добавил диалоговое окно

--- app/src/main/java/com/example/myapplication/LocationActivity.kt
+++ app/src/main/java/com/example/myapplication/LocationActivity.kt
@@ -1,11 +1,56 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
+import com.example.myapplication.databinding.ActivityLocationBinding
 
 class LocationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLocationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_location)
+
+        binding = ActivityLocationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val countryList = arrayOf("Rossia", "Brazil", "Uzbekistan", "Poetugal", "England")
+        val adapter = ArrayAdapter(this@LocationActivity, android.R.layout.simple_list_item_1, countryList)
+
+        binding.locationList.setAdapter(adapter)
+
+        binding.locationList.setOnItemClickListener { parent, view, position, l ->
+            val selectedLocation = parent.getItemAtPosition(position) as String
+            showDialog(selectedLocation)
+        }
+    }
+
+    private fun showDialog(location: String) {
+        val dialogView = layoutInflater.inflate(R.layout.alert_dialog, null)
+        val dialogbuilder = AlertDialog.Builder(this@LocationActivity)
+        dialogbuilder.setView(dialogView)
+        val dialog = dialogbuilder.create()
+
+        dialogView.findViewById<AppCompatButton>(R.id.btn_yes).setOnClickListener {
+            startActivityWithLocation(location)
+        }
+
+        dialogView.findViewById<AppCompatButton>(R.id.btn_cancel).setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.show()
+    }
+
+    private fun startActivityWithLocation(location: String) {
+        val intent = Intent(this@LocationActivity, MainActivity :: class.java)
+        startActivity(intent)
     }
 }
