ff4b024b53e0ac908a56cd461ae7a48cdbec0b64
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2024-01-29 15:02:52 +0300

    Добавил NavigationBar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        changeFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    changeFragment(HomeFragment())
+                }
+                R.id.cart -> {
+                    changeFragment(CartFragment())
+                }
+                R.id.search -> {
+                    changeFragment(SearchFragment())
+                }
+                R.id.history -> {
+                    changeFragment(HistoryFragment())
+                }
+                R.id.profile -> {
+                    changeFragment(ProfileFragment())
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
